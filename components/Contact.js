a8207c1f3fea4aab2dad8af511e2c84c8ab0dbea
Bartłomiej Sopecki <b.sopecki@gmail.com>
2019-02-06 10:45:15 +0100

    fix problem with render method

--- components/Contact.js
+++ components/Contact.js
@@ -1,32 +1,20 @@
 var Contact = React.createClass({
-        propTypes: {
-            item: React.PropTypes.object.isRequired,
-        },
+    propTypes: {
+        item: React.PropTypes.object.isRequired,
+    },
 
-    }, <
-    div className = {
-        'contactItem'
-    } >
-    <
-    img className = {
-        'contactImage'
+    render: function () {
+        return (<div className={'contactItem'}>
+            <img className={'contactImage'} src={'link-do-obrazka.png'} />
+            <p className={'contactLabel'}>
+                Imię: {this.props.item.firstName}
+            </p>
+            <p className={'contactLabel'}>
+                Nazwisko: {this.props.item.lastName}
+            </p>
+            <a href={'mailto:' + this.props.item.email}>
+                {this.props.item.email}
+            </a>
+        </div>)
     }
-    src = {
-        'https://img.freepik.com/darmowe-ikony/u%C5%BCytkownik-wype%C5%82nia-osoba-kszta%C5%82t_318-74922.jpg?size=338c&ext=jpg'
-    }
-    /> <
-    p className = {
-        'contactLabel'
-    } >
-    Imię: {
-        this.props.contact.firstName
-    } <
-    /p> <
-    a href = {
-        'mailto:' + this.props.item.email
-    } > {
-        this.props.item.email
-    } <
-    /a> < /
-    div >
-);
+})
